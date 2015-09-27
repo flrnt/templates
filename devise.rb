19ef7152a49e497cfd497a38a8381cfff621db62
Florent Allain <allain.florent@gmail.com>
2015-09-27 21:38:14 +0200

    Create devise template

---
+++ devise.rb
@@ -0,0 +1,119 @@
+run "rm Gemfile"
+file 'Gemfile', <<-RUBY
+source 'https://rubygems.org'
+ruby '2.2.3'
+
+gem 'rails', '4.2.3'
+gem 'pg'
+gem 'figaro'
+gem 'jbuilder', '~> 2.0'
+gem 'devise'
+
+gem 'sass-rails', '~> 5.0'
+gem 'jquery-rails'
+gem 'uglifier'
+gem 'bootstrap-sass'
+gem 'font-awesome-sass'
+gem 'simple_form'
+gem 'autoprefixer-rails'
+
+group :development, :test do
+  gem 'binding_of_caller'
+  gem 'better_errors'
+  gem 'quiet_assets'
+  gem 'pry-byebug'
+  gem 'pry-rails'
+  gem 'spring'
+end
+
+group :production do
+  gem 'rails_12factor'
+  gem 'puma'
+end
+RUBY
+
+file 'Procfile', <<-YAML
+web: bundle exec puma -C config/puma.rb
+YAML
+
+file 'config/puma.rb', <<-RUBY
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
+RUBY
+
+generate(:controller, 'pages', 'home', '--no-helper', '--no-assets', '--skip-routes')
+route "root to: 'pages#home'"
+
+run 'rm app/assets/javascripts/application.js'
+file 'app/assets/javascripts/application.js', <<-JS
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require_tree .
+JS
+
+run 'rm app/views/layouts/application.html.erb'
+file 'app/views/layouts/application.html.erb', <<-HTML
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>TODO</title>
+    <%= stylesheet_link_tag    'application', media: 'all' %>
+    <%= csrf_meta_tags %>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  </head>
+  <body>
+    <%= render 'shared/flashes' %>
+    <%= yield %>
+    <%= javascript_include_tag 'application' %>
+  </body>
+</html>
+HTML
+
+file 'app/views/shared/_flashes.html.erb', <<-HTML
+<% if notice %>
+  <div class="alert alert-info alert-dismissible" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    <%= notice %>
+  </div>
+<% end %>
+<% if alert %>
+  <div class="alert alert-warning alert-dismissible" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    <%= alert %>
+  </div>
+<% end %>
+HTML
+
+run "README.rdoc"
+file 'README.md', <<-MARKDOWN
+Product by [flrnt](#)
+MARKDOWN
+
+after_bundle do
+  run "bundle exec figaro install"
+  generate('simple_form:install', '--bootstrap')
+  generate('devise:install')
+  generate('devise', 'User')
+  generate('devise:views')
+  environment 'config.action_mailer.default_url_options = { host: "http://localhost:3000" }', env: 'development'
+  environment 'config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }', env: 'production'
+  rake 'db:drop db:create db:migrate'
+  git :init
+  git add: "."
+  git commit: %Q{ -m 'initial commit' }
+end
