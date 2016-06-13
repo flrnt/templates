0ece89c50783c908f60262e85204a6de3e937bd8
flrnt <flrnt@users.noreply.github.com>
2016-06-13 13:46:23 +0200

    Update basic.rb

--- basic.rb
+++ basic.rb
@@ -1,92 +1,78 @@
+run "pgrep spring | xargs kill -9"
 run "rm Gemfile"
 file 'Gemfile', <<-RUBY
 source 'https://rubygems.org'
-ruby '2.3.0'
+ruby '#{RUBY_VERSION}'
 
-gem 'rails', '4.2.6'
+gem 'rails', '#{Rails.version}'
+gem 'puma'
 gem 'pg'
 gem 'figaro'
 gem 'jbuilder', '~> 2.0'
+gem 'redis'
 
-gem 'sass-rails', '~> 5.0'
+gem 'sass-rails'
 gem 'jquery-rails'
 gem 'uglifier'
-gem 'bootstrap-sass'
 gem 'font-awesome-sass'
-gem 'simple_form'
+gem 'simple_form'#{Rails.version >= "5" ? ", github: 'plataformatec/simple_form'" : nil}
 gem 'autoprefixer-rails'
-gem 'normalize-rails', '~> 3.0.3'
 
 group :development, :test do
   gem 'binding_of_caller'
   gem 'better_errors'
-  gem 'quiet_assets'
+  #{Rails.version >= "5" ? nil : "gem 'quiet_assets'"}
   gem 'pry-byebug'
   gem 'pry-rails'
   gem 'spring'
+  #{Rails.version >= "5" ? "gem 'listen', '~> 3.0.5'" : nil}
+  #{Rails.version >= "5" ? "gem 'spring-watcher-listen', '~> 2.0.0'" : nil}
 end
 
 group :production do
   gem 'rails_12factor'
-  gem 'puma'
 end
 RUBY
+
+file ".ruby-version", RUBY_VERSION
 
 file 'Procfile', <<-YAML
 web: bundle exec puma -C config/puma.rb
 YAML
 
-file 'config/puma.rb', <<-RUBY
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['MAX_THREADS'] || 5)
-threads threads_count, threads_count
+if Rails.version < "5"
+puma_file_content = <<-RUBY
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 
-preload_app!
-
-rackup      DefaultRackup
-port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
-
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
-end
+threads     threads_count, threads_count
+port        ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { "development" }
 RUBY
 
-generate(:controller, 'pages', 'home', '--no-helper', '--no-assets', '--skip-routes')
-route "root to: 'pages#home'"
-
-run 'rm app/assets/stylesheets/application.css'
-file 'app/assets/stylesheets/application.css', <<-CSS
-/*
-*= require normalize-rails
-*= require_self
-*= require_tree .
-*/
-CSS
+file 'config/puma.rb', puma_file_content, force: true
+end
 
 run 'rm app/assets/javascripts/application.js'
 file 'app/assets/javascripts/application.js', <<-JS
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets
 //= require_tree .
 JS
+
+gsub_file('config/environments/development.rb', /config\.assets\.debug.*/, 'config.assets.debug = false')
 
 run 'rm app/views/layouts/application.html.erb'
 file 'app/views/layouts/application.html.erb', <<-HTML
 <!DOCTYPE html>
 <html>
   <head>
-    <title>TODO</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    <%= stylesheet_link_tag    'application', media: 'all' %>
-    <%= csrf_meta_tags %>
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>TODO</title>
+    <%= csrf_meta_tags %>
+    #{Rails.version >= "5" ? "<%= action_cable_meta_tag %>" : nil}
+    <%= stylesheet_link_tag    'application', media: 'all' %>
   </head>
   <body>
     <%= yield %>
@@ -95,15 +81,29 @@ file 'app/views/layouts/application.html.erb', <<-HTML
 </html>
 HTML
 
-run "README.rdoc"
-file 'README.md', <<-MARKDOWN
+run "rm README.rdoc"
+markdown_file_content = <<-MARKDOWN
 Product by [flrnt](#)
 MARKDOWN
+file 'README.md', markdown_file_content, force: true
 
 after_bundle do
-  run "bundle exec figaro install"
-  generate('simple_form:install', '--bootstrap')
   rake 'db:drop db:create db:migrate'
+  generate('simple_form:install')
+  generate(:controller, 'pages', 'home', '--no-helper', '--no-assets', '--skip-routes')
+  route "root to: 'pages#home'"
+
+  run "rm .gitignore"
+  file '.gitignore', <<-TXT
+.bundle
+log/*.log
+tmp/**/*
+tmp/*
+*.swp
+.DS_Store
+public/assets
+TXT
+  run "bin/figaro install"
   git :init
   git add: "."
   git commit: %Q{ -m 'initial commit' }
